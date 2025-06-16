e69b6474d1c56f1e48257797d511fffdd8c1db55
Volga <kurvan1112@gmail.com>
2025-06-16 16:44:30 +0900

    Added google.com as match target

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.google.co.jp/maps/*
+// @match        https://www.google.com/maps/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mozilla.org
 // @grant        none
 // ==/UserScript==
